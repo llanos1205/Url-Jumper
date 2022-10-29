411fe2b119ebfe3107bca4fdd8d8f0ba616cecce
llanos1205 <llanos1205@gmail.com>
2022-10-29 18:01:10 -0400

    added s3 push

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region     = "us-east-1"
 }
+module "www-bucket" {
+  source = "./s3"
+  domain_name = "llanoscorp.com"
+  bucket_name = "llanoscorp.com"
+  common_tags = {
+    Project = "llanoscorp"
+  }
+}
---
+++ infra/terraform/s3/terraform.tfvars
@@ -0,0 +1,6 @@
+domain_name = "llanoscorp.com"
+bucket_name = "llanoscorp.com"
+
+common_tags = {
+  Project = "llanoscorp"
+}
