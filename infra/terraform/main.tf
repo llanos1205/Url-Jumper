4dcc740f2280a68d687ff2e281bde091d6c41d93
llanos1205 <llanos1205@gmail.com>
2022-10-30 16:26:57 -0400

    changes in infra sync with tf cloud

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 module "www-bucket" {
-  source = "./s3"
-  domain_name = "llanoscorp.com"
-  bucket_name = "llanoscorp.com"
+  source         = "./s3"
+  domain_name    = var.s3_domain_name
+  bucket_name    = var.s3_bucket_name
   common_tags = {
-    Project = "llanoscorp"
+    Project     = "url-jumper"
+    Environment = var.tf_environment
   }
 }
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "tf_environment" {
+  description = "Common tags you want applied to all components."
+}
+
+variable "s3_domain_name" {
+  type = string
+  description = "The domain name for the website."
+}
+
+variable "s3_bucket_name" {
+  type = string
+  description = "The name of the bucket without the sub-domain prefix. Normally domain_name."
+}
+
+variable "s3_common_tags" {
+  description = "Common tags you want applied to all components."
+}
+
